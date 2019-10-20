d2c807cc135cd99c6d0c51821c5c2067d67083e1
u0_a124 <coluna123@protonmail.ch>
2019-10-20 18:51:51 -0200

    added more rust lesson files

--- rust_book/generics/target/release/deps/generics-5d92b412159149c2.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/rust/aulas_rust/rust_book/generics/target/release/deps/generics-5d92b412159149c2: src/main.rs
-
-/data/data/com.termux/files/home/rust/aulas_rust/rust_book/generics/target/release/deps/generics-5d92b412159149c2.d: src/main.rs
-
-src/main.rs:
--- rust_book/generics/target/release/generics.d
+++
@@ -1,1 +0,0 @@
-/data/data/com.termux/files/home/rust/aulas_rust/rust_book/generics/target/release/generics: /data/data/com.termux/files/home/rust/aulas_rust/rust_book/generics/src/main.rs
