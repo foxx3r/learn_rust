7f761b0582b863e68d70b22e0c360d6bdc54bdc7
foxxer <coluna123@protonmail.ch>
2019-10-22 18:46:42 +0000

    added new lessons

---
+++ rust_book/workspaces/addone/src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub fn add_one(num: i64) -> i64 {
+    return num + 1;
+}
---
+++ rust_book/workspaces/src/main.rs
@@ -0,0 +1,7 @@
+use addone;
+
+fn main() {
+    let num: i64 = 8;
+
+    println!("{} + 1 = {}", num, addone::add_one(num));
+}
